4ac33f55cb9d9a44f94b2d2252e445ab7691bca0
ascherdakova <ch.a.s.1997@mail.ru>
2016-07-09 23:13:28 +0300

    Удаление
    Модификации в этой ветке больше не требуются

--- DBOrganizationProject/DBOrganizationProject/Record.cpp
+++
@@ -1,9 +0,0 @@
-#include"stdafx.h"
-void Record(FOLDER * CurrentFolder, FILE * file)
-{
-	if (CurrentFolder == NULL)
-		return;
-	fprintf(file, "%s\n\n%s\n\n", CurrentFolder->UpFolder->FolderName, CurrentFolder->FolderName);
-	Record(CurrentFolder->NextFolder, file);
-	Record(CurrentFolder->DownFolder, file);
-}
